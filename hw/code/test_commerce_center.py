cfff8cb0fad13f1c8f45c5e6dc656100fbbf5b67
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2025-05-15 00:19:54 +0300

    add close popup test

--- hw/code/test_commerce_center.py
+++ hw/code/test_commerce_center.py
@@ -1,3 +1,4 @@
+import time
 from base import BaseCase
 from ui.pages.commerce_center_page import CommerceCenterPage
 from ui.locators.commerce_center_locators import CommerceCenterPageLocators
@@ -12,3 +13,11 @@ class TestCommerceCenter(BaseCase):
         commerce_center_page.close_popup()
         training_variants = commerce_center_page.find_all(CommerceCenterPageLocators.TRAINING_OFFER_POPUP_TRAIN_BUTTONS)
         assert 'Создать каталог с подсказками' in training_variants[0].text and 'Смотреть видеоурок от экспертов VK' in training_variants[1].text and 'Смотреть курс на обучающей платформе' in training_variants[2].text
+
+    def test_close_modal_education(self, commerce_center_page: CommerceCenterPage):
+        if commerce_center_page.popup_active():  # Sometimes modal windows with the offering of training is shown. We need to close it.
+            commerce_center_page.close_popup()
+        commerce_center_page.click_undergo_training()
+        commerce_center_page.close_popup()
+        popup = commerce_center_page.find(CommerceCenterPageLocators.CURRENT_POPUP)
+        assert popup
