79c9bdf83c0bc0474713e62d319f892165bb55ad
JolyBI3ll <asman2003pda45@gmail.com>
2025-05-15 19:11:41 +0300

    fix env

--- hw/code/conftest.py
+++ hw/code/conftest.py
@@ -1,29 +1,39 @@
 from ui.fixtures import *
+from os import environ
+import pytest
+import os
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser', default='chrome')
-    parser.addoption('--url', default='https://ads.vk.com/')
-    parser.addoption('--debug_log', action='store_true')
-    parser.addoption('--selenoid', action='store_true')
-    parser.addoption('--vnc', action='store_true')
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--url", default="https://ads.vk.com/")
+    parser.addoption("--debug_log", action="store_true")
+    parser.addoption("--selenoid", action="store_true")
+    parser.addoption("--vnc", action="store_true")
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def config(request):
-    browser = request.config.getoption('--browser')
-    url = request.config.getoption('--url')
-    debug_log = request.config.getoption('--debug_log')
+    browser = request.config.getoption("--browser")
+    url = request.config.getoption("--url")
+    debug_log = request.config.getoption("--debug_log")
 
-    users_list_path = environ.get("USERS_LIST_PATH", r"C:\Users\Andrey\Desktop\тп\3 сем\homework-4-spring-2025\hw\code\files\email.txt")
-    keywords_path = environ.get("KEYWORDS_PATH", r"C:\Users\Andrey\Desktop\тп\3 сем\homework-4-spring-2025\hw\code\files\keywords.txt")
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    users_list_path = environ.get(
+        "USERS_LIST_PATH", os.path.join(base_dir, "files", "email.txt")
+    )
+
+    keywords_path = environ.get(
+        "KEYWORDS_PATH", os.path.join(base_dir, "files", "keywords.txt")
+    )
 
     return {
-        'browser': browser,
-        'url': url,
-        'debug_log': debug_log,
-        'selenoid': None,
-        'vnc': False,
-        'users_list_path': users_list_path,
-        'keywords_path': keywords_path
+        "browser": browser,
+        "url": url,
+        "debug_log": debug_log,
+        "selenoid": None,
+        "vnc": False,
+        "users_list_path": os.path.abspath(users_list_path),
+        "keywords_path": os.path.abspath(keywords_path),
     }
