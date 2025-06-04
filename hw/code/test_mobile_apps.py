10d9149539cd06a166b57a7822479d813a98d8b6
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2025-06-04 20:26:35 +0300

    fix test_mobile_apps

--- hw/code/test_mobile_apps.py
+++ hw/code/test_mobile_apps.py
@@ -4,6 +4,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestSitesPage(BaseCase):
+    APP_LINK_INPUT_VALUE = "https://vk.com/"
+    WRONG_APP_LINK_TEXT = "Введите корректную ссылку на приложение"
+
     def test_modal_shows(self, mobile_apps_page: MobileAppsPage):
         mobile_apps_page.click(mobile_apps_page.locators.ADD_APP_BUTTON)
         assert mobile_apps_page.modal_active()
@@ -11,12 +14,15 @@ class TestSitesPage(BaseCase):
     def test_invalid_link(self, mobile_apps_page: MobileAppsPage):
         mobile_apps_page.click(mobile_apps_page.locators.ADD_APP_BUTTON)
         mobile_apps_page.fill_in(
-            mobile_apps_page.locators.APP_LINK_INPUT, "https://vk.com/"
-        )
-        mobile_apps_page.wait(timeout=10).until(
-            EC.visibility_of_element_located(mobile_apps_page.locators.ALERT_TEXT)
+            mobile_apps_page.locators.APP_LINK_INPUT, self.APP_LINK_INPUT_VALUE
         )
 
         mobile_apps_page.click(mobile_apps_page.locators.MODAL_ADD_APP_BUTTON)
 
-        assert len(mobile_apps_page.find_all(mobile_apps_page.locators.ALERT_TEXT)) > 0
+        mobile_apps_page.wait(timeout=10).until(
+            EC.visibility_of_element_located(mobile_apps_page.locators.ALERT_TEXT)
+        )
+
+        alert_text = mobile_apps_page.find(mobile_apps_page.locators.ALERT_TEXT).text
+
+        assert alert_text == self.WRONG_APP_LINK_TEXT
