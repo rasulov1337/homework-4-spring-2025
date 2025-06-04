cd6ef7efed50a4a670dd6c5c62fbf56c09414279
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2025-06-04 18:34:27 +0300

    move text data into consts

--- hw/code/test_company.py
+++ hw/code/test_company.py
@@ -3,32 +3,33 @@ from ui.pages.company_page import CompanyPage, CompanyTarget
 
 
 class TestCompany(BaseCase):
+
+    SITE_URL = "https://pootnick.ru/"
+    AD_HEADER = "AD"
+    AD_DESC = "DESC"
+    ADVERTISED_SITE_TEXT = "Рекламируемый сайт"
+
     def test_create_site_company(self, company_page: CompanyPage):
         company_page.close_help_modal()
         company_page.open_company_creation()
         company_page.select_target(CompanyTarget.SITE)
-        assert "Рекламируемый сайт" in company_page.driver.page_source
+        assert self.ADVERTISED_SITE_TEXT in company_page.driver.page_source
 
-        site_url = 'https://pootnick.ru/'
-
-        company_page.set_site_url(site_url)
-        assert site_url in company_page.driver.page_source
+        company_page.set_site_url(self.SITE_URL)
+        assert self.SITE_URL in company_page.driver.page_source
         company_page.unfocus()
         company_page.apply_target()
 
         company_page.click_next()
         company_page.check_not_click_next()
         company_page.set_region()
-        assert 'Россия' in company_page.driver.page_source
+        assert "Россия" in company_page.driver.page_source
         company_page.click_next()
 
-        ad_header = 'AD'
-        ad_desc = 'DESC'
-
-        company_page.set_ad_header(ad_header)
-        company_page.set_ad_short_desc(ad_desc)
-        assert ad_header in company_page.driver.page_source
-        assert ad_desc in company_page.driver.page_source
+        company_page.set_ad_header(self.AD_HEADER)
+        company_page.set_ad_short_desc(self.AD_DESC)
+        assert self.AD_HEADER in company_page.driver.page_source
+        assert self.AD_DESC in company_page.driver.page_source
 
         company_page.save_company()
 
