f5632414817b82d7e61b88248ce21667642d327c
Qaleka <91965947+Qaleka@users.noreply.github.com>
2025-06-04 19:06:43 +0300

    done setup

--- hw/code/test_audience.py
+++ hw/code/test_audience.py
@@ -3,6 +3,26 @@ from ui.pages.audience_page import AudiencePage, AudienceSource
 
 
 class TestAudience(BaseCase):
+    def setup_method(self, method):
+        if method.__name__ == 'test_audience_from_existing_audience':
+            self.audience_page = AudiencePage(self.driver)
+
+            users_list_name = "USER LIST"
+            users_list_type = "Email"
+            users_list_path = self.config['users_list_path']
+
+            audience_name = 'EXISTING_AUDIENCE'
+            self.audience_page.open_audience_creation()
+            self.audience_page.set_audience_name(audience_name)
+            self.audience_page.open_sources_list()
+            self.audience_page.select_audience_source(AudienceSource.USERS_LIST)
+            self.audience_page.click_upload_new_users_list()
+            self.audience_page.load_new_users_list(users_list_name, users_list_type, users_list_path)
+            self.audience_page.submit_audience_source()
+            self.audience_page.wait_for_success_notify()
+            self.audience_page.submit_audience_creation()
+            self.audience_page.wait_audience_list_for_load()
+
     def teardown_method(self, method):
         AudiencePage(self.driver).open_users_list_list()
         AudiencePage(self.driver).clear_users_lists()
@@ -77,46 +97,21 @@ class TestAudience(BaseCase):
         assert len(audiences) == 1
         assert audience_name in audiences
 
-        audience_page.clear_audiences()
-
-    def test_audience_from_existing_audience(self, audience_page: AudiencePage):
-        audience_page.open_audience_creation()
-
-        audience_name = 'EXISTING_AUDIENCE'
-        audience_page.set_audience_name(audience_name)
-        audience_page.open_sources_list()
-        audience_page.select_audience_source(AudienceSource.USERS_LIST)
-
-        users_list_name = "USER LIST"
-        users_list_type = "Email"
-        users_list_path = self.config['users_list_path']
-
-        audience_page.click_upload_new_users_list()
-
-        audience_page.load_new_users_list(users_list_name, users_list_type, users_list_path)
-        audience_page.submit_audience_source()
-        audience_page.wait_for_success_notify()
-        audience_page.submit_audience_creation()
-
-        audiences = audience_page.get_audiences()
-        assert len(audiences) == 1
-        assert audience_name in audiences
-
-        audience_page.open_audience_creation()
-
+    def test_audience_from_existing_audience(self):
         audience_name = 'AUDIENCE'
-        audience_page.set_audience_name(audience_name)
 
-        audience_page.open_sources_list()
-        audience_page.select_audience_source(AudienceSource.EXISTING)
+        self.audience_page.open_audience_creation()
+        self.audience_page.set_audience_name(audience_name)
+        self.audience_page.open_sources_list()
+        self.audience_page.select_audience_source(AudienceSource.EXISTING)
 
-        audience_page.add_existing_audience("EXISTING_AUDIENCE")
-        assert "EXISTING_AUDIENCE" in audience_page.driver.page_source
-        audience_page.submit_audience_source()
-        assert "Уже созданная аудитория" in audience_page.driver.page_source
-        audience_page.submit_audience_creation()
+        self.audience_page.add_existing_audience("EXISTING_AUDIENCE")
+        assert "EXISTING_AUDIENCE" in self.audience_page.driver.page_source
+        self.audience_page.submit_audience_source()
+        assert "Уже созданная аудитория" in self.audience_page.driver.page_source
+        self.audience_page.submit_audience_creation()
 
-        audience_page.wait_audience_list_for_load()
-        audiences = audience_page.get_audiences()
+        self.audience_page.wait_audience_list_for_load()
+        audiences = self.audience_page.get_audiences()
         assert len(audiences) == 2
         assert audience_name in audiences
