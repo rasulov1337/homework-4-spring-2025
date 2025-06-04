0921eb3feb8cbd04537810a9afa48674eac378a4
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2025-06-04 22:16:24 +0300

    add teardown method for test sites

--- hw/code/test_sites.py
+++ hw/code/test_sites.py
@@ -3,7 +3,6 @@ from ui.pages.sites_page import SitesPage
 from selenium.webdriver.support import expected_conditions as EC
 from faker import Faker
 
-
 fake = Faker()
 
 
@@ -41,13 +40,37 @@ class TestSitesPage(BaseCase):
         sites_page.click_add_pixel()
         sites_page.fill_in(sites_page.locators.SITE_DOMAIN_INPUT, random_site)
         sites_page.click_create_pixel()
+
         sites_page.wait(timeout=60).until(
             EC.visibility_of_element_located(sites_page.locators.PIXEL_CREATED_TEXT),
         )
+
         sites_page.click(sites_page.locators.GET_CODE_BUTTON)
 
         sites_page.wait(10).until(
             lambda d: len(d.window_handles) > 1
         )  # Wait for new tab to open
-        # self.driver.switch_to.window(self.driver.window_handles[1])
-        # assert random_site in self.driver.find_element(sites_page.locators.BODY).text
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        site_url_text = self.driver.find_element(
+            sites_page.locators.PIXEL_URL_ANCHOR
+        ).text
+        assert random_site in site_url_text
+
+    def teardown_method(self, method):
+        try:
+            sites_page = SitesPage(self.driver)
+
+            buttons = sites_page.find_all(sites_page.locators.PIXEL_MORE_BUTTON)
+            for button in buttons:
+                button.click()
+                sites_page.click_delete_pixel()
+                sites_page.confirm_pixel_delition()
+                sites_page.wait(30).until(
+                    EC.invisibility_of_element_located(
+                        sites_page.locators.CONFIRM_DELITION_OVERLAY
+                    )
+                )
+
+            sites_page.page.delete_catalog_by_name(self.CATALOG_NAME_TEXT)
+        except Exception:
+            pass
