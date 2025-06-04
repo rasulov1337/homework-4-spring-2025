1d4a56e7c58e10bfbf04723611097f8b71974a9e
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2025-06-04 20:00:44 +0300

    mark test login and save cookies to be skipped

--- hw/code/test_login_and_save_cookies.py
+++ hw/code/test_login_and_save_cookies.py
@@ -3,30 +3,34 @@
 from selenium import webdriver
 import json
 import time
+import pytest
 
+
+@pytest.mark.skip("It is intended to be run only once")
 def test_login_and_save_data():
     driver = webdriver.Chrome()
     driver.get("https://ads.vk.com/")
 
     time_to_login = 120
 
-    print("Войдите в аккаунт вручную. localStorage и куки будут сохранены. У вас есть: {} секунд. Время пошло.".format(time_to_login))
+    print(
+        "Войдите в аккаунт вручную. localStorage и куки будут сохранены. У вас есть: {} секунд. Время пошло.".format(
+            time_to_login
+        )
+    )
     time.sleep(time_to_login)  # время на ручной вход
 
     # --- Сохраняем куки ---
     cookies = driver.get_cookies()
     for cookie in cookies:
-        cookie.pop('sameSite', None)
-        cookie.pop('expiry', None)
+        cookie.pop("sameSite", None)
+        cookie.pop("expiry", None)
 
     # --- Сохраняем localStorage ---
     local_storage = driver.execute_script("return window.localStorage;")
-    
+
     # Сохраняем всё в JSON
     with open("session_data.json", "w") as f:
-        json.dump({
-            "cookies": cookies,
-            "localStorage": local_storage
-        }, f)
+        json.dump({"cookies": cookies, "localStorage": local_storage}, f)
 
     driver.quit()
