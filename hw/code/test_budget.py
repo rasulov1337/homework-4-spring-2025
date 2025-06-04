ff255afc28158b1122ceb977d81e1b2d7fc3d5ef
FireSpirit171 <rhodyrev7@gmail.com>
2025-06-05 00:17:31 +0300

    fix

--- hw/code/test_budget.py
+++ hw/code/test_budget.py
@@ -3,11 +3,15 @@ from ui.pages.budget_page import BudgetPage
 
 
 class TestBudget(BaseCase):
+    PAY_SUM_TEXT = "Cумма к оплате"
+    MINIMAL_SUM_TEXT = "Минимальная сумма 600,00 ₽"
+    SUM_TO_BALANCE = "На баланс поступит (НДС — 20%)"
+
     def test_open_replenishment_modal_page(self, budget_page: BudgetPage):
         budget_page.click_replenish_budget_button()
         assert budget_page.is_replenishment_modal_visible()
-        assert budget_page.is_replenishment_modal_field_visible("Cумма к оплате")
-        assert budget_page.is_replenishment_modal_field_visible("На баланс поступит (НДС — 20%)")
+        assert budget_page.is_replenishment_modal_field_visible(self.PAY_SUM_TEXT)
+        assert budget_page.is_replenishment_modal_field_visible(self.SUM_TO_BALANCE)
 
     def test_valid_amount(self, budget_page):
         budget_page.click_replenish_budget_button()
@@ -25,11 +29,11 @@ class TestBudget(BaseCase):
 
     def test_error_too_small_amount(self, budget_page):
         budget_page.click_replenish_budget_button()
-        budget_page.enter_amount('599')
-        assert budget_page.get_error_message() == 'Минимальная сумма 600,00 ₽'
+        budget_page.enter_amount("599")
+        assert budget_page.get_error_message() == self.MINIMAL_SUM_TEXT
 
     def test_error_too_small_amount_without_vat(self, budget_page):
         budget_page.click_replenish_budget_button()
-        budget_page.enter_amount_without_vat('499')
+        budget_page.enter_amount_without_vat("499")
         budget_page.click_submit_button()
-        assert budget_page.get_error_message() == 'Минимальная сумма 600,00 ₽'
+        assert budget_page.get_error_message() == self.MINIMAL_SUM_TEXT
