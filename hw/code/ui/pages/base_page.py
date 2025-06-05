cf022233891e41d990276daf5ec0f9a7413b9602
Qaleka <91965947+Qaleka@users.noreply.github.com>
2025-06-05 13:04:16 +0300

    delete base page func

--- hw/code/ui/pages/base_page.py
+++ hw/code/ui/pages/base_page.py
@@ -1,23 +1,24 @@
 import time
+from functools import wraps
 
 from selenium.common import TimeoutException
 from selenium.webdriver import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from .base_page_functionality import BasePageFunctionality
 from ui.locators.base_page_locators import BasePageLocators
 
-from functools import wraps
+
+BASIC_TIMEOUT = 10
 
 
 class PageNotOpenedException(Exception):
     pass
 
 
-class BasePage(BasePageFunctionality):
+class BasePage:
     url = "https://ads.vk.com/"
     locators = BasePageLocators()
 
@@ -34,98 +35,125 @@ class BasePage(BasePageFunctionality):
             f"{self.url} did not open in {timeout} sec, current url {self.driver.current_url}"
         )
 
-    def wait(self, timeout=None):
-        if timeout is None:
-            timeout = 5
+    def wait(self, timeout=BASIC_TIMEOUT) -> WebDriverWait:
         return WebDriverWait(self.driver, timeout=timeout)
 
-    def find(self, locator, timeout=None):
-        return self.wait(timeout).until(EC.presence_of_element_located(locator))
+    def find(self, locator, timeout=BASIC_TIMEOUT, until=EC.presence_of_element_located) -> WebElement:
+        return self.wait(timeout).until(until(locator))
 
-    def find_visibility(self, locator, timeout=None):
-        return self.wait(timeout).until(EC.visibility_of_element_located(locator))
+    def find_visibility(self, locator, timeout=BASIC_TIMEOUT) -> WebElement:
+        return self.find(locator, timeout, until=EC.visibility_of_element_located)
 
-    def find_all(self, locator, timeout=None) -> list[WebElement]:
+    def find_all(self, locator, timeout=BASIC_TIMEOUT) -> list[WebElement]:
         return self.wait(timeout).until(EC.visibility_of_all_elements_located(locator))
 
-    def find_all_presence(self, locator, timeout=None) -> list[WebElement]:
+    def find_all_presence(self, locator, timeout=BASIC_TIMEOUT) -> list[WebElement]:
         return self.wait(timeout).until(EC.presence_of_all_elements_located(locator))
 
-    def find_interactable(self, locator, timeout=None):
+    def find_interactable(self, locator, timeout=BASIC_TIMEOUT) -> WebElement:
         return self.wait(timeout).until(EC.element_to_be_clickable(locator))
 
-    def click(self, locator, timeout=None) -> WebElement:
-        self.find(locator, timeout=timeout)
-        elem = self.wait(timeout).until(EC.element_to_be_clickable(locator))
-        elem.click()
-
-    def clear(self, locator, timeout: float | None = None) -> WebElement:
-        elem = self.find(locator, timeout)
-        # elem.clear()
-
-        if elem.get_attribute("value") != "":
-            size = len(elem.get_attribute("value"))
-            elem.send_keys(size * Keys.BACKSPACE)
-
-        return elem
-
-    def fill(self, locator, keys):
-        self.find(locator).send_keys(keys)
-
-    def fill_in(self, locator, query: str, timeout: float | None = None) -> WebElement:
-        elem = self.clear(locator, timeout)
-        elem.send_keys(query)
-        return elem
-
-    def find_invisible(self, locator, timeout=None) -> WebElement:
+    def find_invisible(self, locator, timeout=BASIC_TIMEOUT) -> WebElement:
         return self.wait(timeout).until(EC.presence_of_element_located(locator))
 
-    def is_visible(self, locator, timeout=None):
-        try:
-            self.wait(timeout).until(EC.visibility_of_element_located(locator))
-            return True
-        except TimeoutException:
-            return False
+    def find_child(self, parent, child_locator, timeout=BASIC_TIMEOUT, until=EC.presence_of_element_located):
+        return WebDriverWait(parent, timeout).until(until(child_locator))
 
-    def go_to_new_tab(self):
-        handles = self.driver.window_handles
-        assert len(handles) > 1
-        self.driver.switch_to.window(handles[1])
+    def clear(self, locator, timeout=BASIC_TIMEOUT) -> WebElement:
+        elem = self.find(locator, timeout)
+        value = elem.get_attribute("value")
+        if value:
+            elem.send_keys(len(value) * Keys.BACKSPACE)
+        return elem
 
-    def became_visible(self, locator, timeout=None):
-        try:
-            self.wait(timeout).until(EC.visibility_of_element_located(locator))
-            return True
-        except TimeoutException:
-            return False
+    def fill(self, locator, keys, timeout=BASIC_TIMEOUT):
+        self.find(locator, timeout).send_keys(keys)
 
-    def became_invisible(self, locator, timeout=None):
-        try:
-            self.wait(timeout).until(EC.invisibility_of_element_located(locator))
-            return True
-        except TimeoutException:
-            return False
-
-    def hover(self, locator, timeout=5):
-        elem = self.wait(timeout).until(EC.presence_of_element_located(locator))
-        ActionChains(self.driver).move_to_element(elem).perform()
+    def fill_in(self, locator, text: str, timeout=BASIC_TIMEOUT) -> WebElement:
+        elem = self.clear(locator, timeout)
+        elem.send_keys(text)
+        return elem
 
     def fill_field(self, field, value):
         elem = self.find(field)
         elem.clear()
         elem.send_keys(value)
 
+    def click(self, locator, timeout=BASIC_TIMEOUT) -> WebElement:
+        elem = self.wait(timeout).until(EC.element_to_be_clickable(locator))
+        elem.click()
+        return elem
+
+    def hover(self, locator, timeout=BASIC_TIMEOUT):
+        elem = self.find(locator, timeout)
+        ActionChains(self.driver).move_to_element(elem).perform()
+
+    def hover_to_element(self, element):
+        ActionChains(self.driver).move_to_element(element).perform()
+        return element
+
+    def check_url(self, expected_url, timeout=BASIC_TIMEOUT):
+        return self.wait(timeout).until(EC.url_matches(expected_url))
+
+    def is_visible(self, locator, timeout=BASIC_TIMEOUT):
+        try:
+            self.wait(timeout).until(EC.visibility_of_element_located(locator))
+            return True
+        except TimeoutException:
+            return False
+
+    def became_visible(self, locator, timeout=BASIC_TIMEOUT):
+        try:
+            self.wait(timeout).until(EC.visibility_of_element_located(locator))
+            return True
+        except TimeoutException:
+            return False
+
+    def became_invisible(self, locator, timeout=BASIC_TIMEOUT):
+        try:
+            self.wait(timeout).until(EC.invisibility_of_element_located(locator))
+            return True
+        except TimeoutException:
+            return False
+
+    def close_alert_if_shown(self, timeout=BASIC_TIMEOUT):
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.alert_is_present())
+            alert = self.driver.switch_to.alert
+            alert.accept()
+        except Exception:
+            pass
+
+    def go_to_new_tab(self):
+        handles = self.driver.window_handles
+        if len(handles) > 1:
+            self.driver.switch_to.window(handles[1])
+        else:
+            raise Exception("No second tab found")
+
     def modal_active(self):
         return len(self.find_all(self.locators.CURRENT_MODAL)) > 0
 
+    def get_value_from_elem(self, element):
+        return element.get_attribute("value")
 
-class PageWithView(BasePageFunctionality):
-    url = ""
+    def get_value(self, locator, timeout=BASIC_TIMEOUT):
+        element = self.find(locator, timeout)
+        return self.get_value_from_elem(element)
 
-    def open_view(self, button_open_locator, sign_opening_locator):
-        self.click(button_open_locator)
-        self.find_with_check_visibility(sign_opening_locator)
+    def write_input_to_element(self, element, message):
+        element.clear()
+        element.send_keys(message)
+        return element
 
-    def close_view(self, button_close_locator, sign_opening_locator):
-        self.click(button_close_locator)
-        self.find(sign_opening_locator, until_EC=EC.invisibility_of_element_located)
+    def write_input(self, locator, message, timeout=BASIC_TIMEOUT):
+        input_element = self.find_visibility(locator, timeout)
+        input_element = self.write_input_to_element(input_element, message)
+        self.wait().until(EC.text_to_be_present_in_element_value(locator, message))
+        return input_element
+
+    def write_input_without_clearing(self, locator, message, timeout=BASIC_TIMEOUT):
+        input_element = self.find_visibility(locator, timeout)
+        input_element.send_keys(message)
+        self.wait().until(EC.text_to_be_present_in_element_value(locator, message))
+        return input_element
